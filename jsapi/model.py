e97a43bb751c35a1a8f08b58e144253fe2850331
Ville Tuulos <ville@bitdeli.com>
2013-03-28 04:12:38 +0000

    fix model for jsapi

--- jsapi/model.py
+++ jsapi/model.py
@@ -2,6 +2,9 @@ from bitdeli.model import model
 
 @model
 def build(profiles):
-    for profile in profiles:
-        for event in profile['events']:
-            yield event, profile.uid
+    for profile in profiles:        
+        events = frozenset(event['$event_name']
+                           for tstamp, group, ip, event in profile['events'])
+        uid = profile.uid
+        for event in events:
+            yield event, uid
