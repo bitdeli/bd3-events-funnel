9f57c2ade86ced43a62eca94b8fdbec79e4ee3b1
Jyri Tuulos <jyri.tuulos@gmail.com>
2013-04-30 22:33:39 +0000

    include dom events, handle missing event arrays

--- jsapi/model.py
+++ jsapi/model.py
@@ -1,10 +1,17 @@
 from bitdeli.model import model
 
+def get_names(events, name):
+    for tstamp, group, ip, event in events:
+        if name in event:
+            yield event[name]
+
 @model
 def build(profiles):
-    for profile in profiles:        
-        events = frozenset(event['$event_name']
-                           for tstamp, group, ip, event in profile['events'])
+    for profile in profiles:
+        events = set(get_names(profile.get('events', []),
+                               '$event_name'))
+        events.update(get_names(profile.get('$dom_event', []),
+                                '$event_label'))
         uid = profile.uid
         for event in events:
             yield event, uid
